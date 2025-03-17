4c1a319ba1f1351ae0324b642af0f288d20defcc
DawidKlimont <dawid.klimont@gmail.com>
2025-03-17 12:28:38 +0100

    Fixed run.sh

--- partitioned-heat-conduction/solver-modulus/run.sh
+++ partitioned-heat-conduction/solver-modulus/run.sh
@@ -1,5 +1,5 @@
 set -e -u
 . ../../tools/log.sh
 exec > >(tee --append "$LOGFILE") 2>&1
-echo "Running simulation with default FEniCS implementation"
+echo "Running simulation with default Modulus implementation"
 python3 ../solver-modulus/heat.py
